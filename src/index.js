89ee60966e0cd80d6bd910d996814aab25134a7e
Andrea <andrearivas4cinfor@gmail.com>
2023-01-16 16:56:30 +0100

    Add task function and local storage

--- src/index.js
+++ src/index.js
@@ -1,39 +1,59 @@
 import './style.css';
 
 const listContainer = document.querySelector('.todo-list');
+const todoInput = document.querySelector('.todo-input');
+const tasksStored = JSON.parse(localStorage.getItem('tasks'));
+let tasks;
 
-const tasks = [
-  {
-    description: 'Clean house',
-    completed: true,
-    index: 3,
-  },
-  {
-    description: 'Homework',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Shopping',
-    completed: true,
-    index: 0,
-  },
-];
-const displayTasks = function () {
-  const arrsorted = tasks.sort((a, b) => a.index - b.index);
-  for (let i = 0; i < arrsorted.length; i += 1) {
-    const html = `
-    <div class="task-wrapper">
-    <form class="completed-form">
-      <input
-      class="checkbox"
-      type="checkbox"
-      >
-    </form>
-    <p class="task-text"> ${tasks[i].description}</p>
-  </div>`;
-    listContainer.innerHTML += html;
+if(localStorage.getItem('task')=== null){
+  tasks = []
+} else {
+  tasks = JSON.parse(localStorage.getItem('tasks'))
+}
+// const displayTasks = function () {
+//   // const arrsorted = tasks.sort((a, b) => a.index - b.index);
+//   for (let i = 0; i < tasks.length; i += 1) {
+//     const html = `
+//     <div class="task-wrapper">
+//     <form class="completed-form">
+//       <input
+//       class="checkbox"
+//       type="checkbox"
+//       >
+//     </form>
+//     <p class="task-text"> ${tasks[i].description}</p>
+//   </div>`;
+//     listContainer.innerHTML += html;
+// //   }
+// // };
+
+// document.addEventListener('DOMContentLoaded', displayTasks);
+
+const form = document.querySelector('.todo-form');
+
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  const newTaskInput = todoInput.value;
+  addTaskToStorage(newTaskInput);
+  //display task 
+  //clean input 
+  todoInput.value = "";
+  JSON.parse(localStorage.getItem('task'));
+})
+
+
+
+const addTaskToStorage = function(newTaskInput) {
+  //create index
+  let index;
+  const len = tasks.length;
+  if(len === 0 || len === null){
+    index = 0
+  } else {
+    index = len + 1;
   }
-};
-
-document.addEventListener('DOMContentLoaded', displayTasks);
+  //create objext
+  const newTask = {description:newTaskInput, status:false, index};
+  tasks.push(newTask);
+  localStorage.setItem('task', JSON.stringify(tasks));
+}
