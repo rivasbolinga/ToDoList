59edf4a6041e2eaf0be0e7a6a3c3f6c36eb23b6c
Andrea <andrearivas4cinfor@gmail.com>
2023-01-17 20:57:57 +0100

    Fix linters and README update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import { update } from 'lodash';
 import './style.css';
-
+// Declare Global Variables
 const listContainer = document.querySelector('.todo-list');
 const todoInput = document.querySelector('.todo-input');
-const tasksStored = JSON.parse(localStorage.getItem('tasks'));
-const taskContainer = document.querySelectorAll('.task-wrapper');
-const taskText = document.querySelectorAll('.task-text');
+const form = document.querySelector('.todo-form');
 let tasks;
-
-if(localStorage.getItem('tasks')=== null){
-  tasks = []
+//Create array from LS
+if (localStorage.getItem('tasks') === null) {
+  tasks = [];
 } else {
-  tasks = JSON.parse(localStorage.getItem('tasks'))
+  tasks = JSON.parse(localStorage.getItem('tasks'));
 }
+
 const displayTasks = function (storage) {
   // const arrsorted = tasks.sort((a, b) => a.index - b.index);
-    const html = `
+  const html = `
   <div class="task-wrapper">
      <div class="description-container">
       <form class="completed-form">
@@ -37,45 +35,27 @@ const displayTasks = function (storage) {
      </div>
     
     </div>`;
-    listContainer.innerHTML += html;
-  };
+  listContainer.innerHTML += html;
+};
 
-
-const form = document.querySelector('.todo-form');
-
-form.addEventListener('submit', (e)=>{
-  e.preventDefault();
-  //take input value
-  const newTaskInput = todoInput.value;
-  //
-  addTaskToStorage(tasks, newTaskInput);
-   JSON.parse(localStorage.getItem('task'));
- 
-  //clean input 
-  todoInput.value = "";
-  
-})
-
-
-///LOCAL STORAGE
-
-//--Add tasks to Local Storage
-const addTaskToStorage = function(arr,newTaskInput) {
-  //create index
+/// LOCAL STORAGE
+// --Add tasks to Local Storage
+const addTaskToStorage = function (arr, newTaskInput) {
+  // create index
   let index;
   const len = arr.length;
-  if(len === 0 || len === null){
-    index = 0
+  if (len === 0 || len === null) {
+    index = 0;
   } else {
-    index = arr[len -1].index+1;
+    index = arr[len - 1].index + 1;
   }
-  //create objext and push it to array and Ls
-  const newTask = {description:newTaskInput, status:false, index};
+  // create object and push it to array and LS
+  const newTask = { description: newTaskInput, status: false, index };
   tasks.push(newTask);
   localStorage.setItem('tasks', JSON.stringify(tasks));
   displayTasks(newTask);
-}
-//--Remove task from Local Storage
+};
+// --Remove task from Local Storage
 const removeItemfromLs = function (id) {
   let tasks = JSON.parse(localStorage.getItem('tasks'));
   tasks = tasks.filter((e) => e.index.toString() !== id.toString());
@@ -83,38 +63,46 @@ const removeItemfromLs = function (id) {
     tasks[i].index = i + 1;
   }
   localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-const updateLs = function(newInput, id){
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
+};
+// --Update Local Storage when mdoify task
+const updateLs = function (newInput, id) {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
   tasks[id - 1].description = newInput.trim();
   localStorage.setItem('tasks', JSON.stringify(tasks));
- }
- const getnewInput = function (input, tasks, id){
-  const newInput = input.value; 
+};
+/// EVENTS
+//--Event to submit the new task
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // take input value
+  const newTaskInput = todoInput.value;
+  addTaskToStorage(tasks, newTaskInput);
+  JSON.parse(localStorage.getItem('task'));
+  // clean input
+  todoInput.value = '';
+});
+
+const getnewInput = function (input, id) {
+  const newInput = input;
   input.addEventListener('keyup', () => {
-    updateLs(newInput.value, id)
-  })
- }
+    updateLs(newInput.value, id);
+  });
+};
 
-listContainer.addEventListener('click', (e)=>{
-if(e.target.classList.contains('task-text')){
- 
-  const taskTargeted = e.target.parentElement.parentElement.parentElement;
-  taskTargeted.querySelector('.fa-ellipsis-vertical').style.display = 'none';
-  taskTargeted.querySelector('.fa-trash-can').style.display = 'flex';
-  
-  e.target.readOnly = false;
-  const { id } = e.target;
-  const tasks = JSON.parse(localStorage.getItem('tasks'));
-  getnewInput(e.target, tasks, id);
+const clickHandle = function (e) {
+  if (e.target.classList.contains('task-text')) {
+    const taskTargeted = e.target.parentElement.parentElement.parentElement;
+    taskTargeted.querySelector('.fa-ellipsis-vertical').style.display = 'none';
+    taskTargeted.querySelector('.fa-trash-can').style.display = 'flex';
 
-} else if (e.target.classList.contains('fa-trash-can')) {
-  const { id } = e.target;
-  removeItemfromLs(id)
-  e.target.parentElement.parentElement.remove();
-  
-}
- });
-
-
+    e.target.readOnly = false;
+    const { id } = e.target;
+    getnewInput(e.target, id);
+  } else if (e.target.classList.contains('fa-trash-can')) {
+    const { id } = e.target;
+    removeItemfromLs(id);
+    e.target.parentElement.parentElement.remove();
+  }
+};
+//--Event to handle UI in task
+listContainer.addEventListener('click', clickHandle);
